e3799d6b53abe6aa2ba6e55fdfdc03b3ee2f7659
f.staikos7 <f.staikos7@gmail.com>
2023-02-15 11:27:33 +0200

    Added timer and results

--- app/src/main/java/com/example/stroopgame/StroopActivity.java
+++ app/src/main/java/com/example/stroopgame/StroopActivity.java
@@ -1,154 +1,278 @@
 package com.example.stroopgame;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
-import android.annotation.SuppressLint;
-import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.SystemClock;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.example.stroopgame.R;
-import com.example.stroopgame.Stroop.StroopData;
-import com.example.stroopgame.Stroop.StroopTest;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
+import java.util.Locale;
+import java.util.Random;
 
 public class StroopActivity extends AppCompatActivity {
-    private TextView wordDisplay;
+
+    // Declare and initialize variables
+    private TextView wordTextView;
+    private TextView resultTextView;
+    private TextView timeTextView;
     private Button redButton;
     private Button blueButton;
     private Button greenButton;
     private Button yellowButton;
-    private ArrayList<StroopData> stimuli;
-    private int currentStimulusIndex = 0;
-    private ArrayList<StroopData> responses = new ArrayList<>();
+    private CountDownTimer timer;
+    private int correctCount = 0;
+    private int incorrectCount = 0;
+    private int timeoutCount = 0;
+    private int trialNumber = 0;
+    private List<Trial> trialList = new ArrayList<>();
+
+    // Declare words and colors arrays
+    private String[] words = {"RED", "BLUE", "GREEN", "YELLOW"};
+    private int[] colors = {Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stroop);
 
-        wordDisplay = findViewById(R.id.word_text_view);
+        // Get references to UI elements
+        wordTextView = findViewById(R.id.word_text_view);
+        resultTextView = findViewById(R.id.resultTextView);
+        timeTextView = findViewById(R.id.timeTextView);
         redButton = findViewById(R.id.red_button);
         blueButton = findViewById(R.id.blue_button);
         greenButton = findViewById(R.id.green_button);
         yellowButton = findViewById(R.id.yellow_button);
 
+        // Set a 5-minute timer to end the test
+        timer = new CountDownTimer(5 * 60 * 1000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                // Display the remaining time in minutes and seconds
+                int minutes = (int) millisUntilFinished / (60 * 1000);
+                int seconds = (int) (millisUntilFinished / 1000) % 60;
+                String timeLeft = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+                timeTextView.setText("Time left: " + timeLeft);
+            }
+
+            public void onFinish() {
+                // Test has ended, do something here
+                computeTestResults();
+            }
+        }.start();
+
+        // Set the first word and color
+        setRandomWordAndColor();
+
+        // Set listeners for the buttons
         redButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onResponseSelected("Red");
+                onButtonClick("RED", Color.RED);
             }
         });
 
         blueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onResponseSelected("Blue");
+                onButtonClick("BLUE", Color.BLUE);
             }
         });
 
         greenButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onResponseSelected("Green");
+                onButtonClick("GREEN", Color.GREEN);
             }
         });
 
         yellowButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onResponseSelected("Yellow");
+                onButtonClick("YELLOW", Color.YELLOW);
             }
         });
-
-        stimuli = new StroopTest().getStimuli();
-        // shuffle the stimuli to present them in a random order
-        Collections.shuffle(stimuli);
-        // present the first stimulus
-        presentStimulus(stimuli.get(currentStimulusIndex));
     }
 
+    private void setRandomWordAndColor() {
+        // Get a random index for the words and colors arrays
+        int index = new Random().nextInt(words.length);
+        int Colorindex = new Random().nextInt(colors.length);
+        // Set the word text and color
+        wordTextView.setText(words[index]);
+        wordTextView.setTextColor(colors[Colorindex]);
 
-    private void presentStimulus(StroopData stimulus) {
-        wordDisplay.setText(stimulus.getWord());
-        switch (stimulus.getDisplayColor()) {
-            case "Red":
-                wordDisplay.setTextColor(ContextCompat.getColor(this,R.color.red));
-                break;
-            case "Blue":
-                wordDisplay.setTextColor(ContextCompat.getColor(this,R.color.blue));
-                break;
-            case "Green":
-                wordDisplay.setTextColor(ContextCompat.getColor(this,R.color.green));
-                break;
-            case "Yellow":
-                wordDisplay.setTextColor(ContextCompat.getColor(this,R.color.yellow));
-                break;
-        }
+        // Set the Stroop color match
+        boolean isMatch = (words[index].equals(getColorName(colors[index])));
+        trialNumber++;
+        Trial trial = new Trial(trialNumber, words[index], getColorName(colors[index]), isMatch, 0, 0, 0);
+        trialList.add(trial);
     }
 
-    private void onResponseSelected(String color) {
-        long responseTime = SystemClock.elapsedRealtime();
-        StroopData currentStimulus = stimuli.get(currentStimulusIndex);
-        currentStimulus.setResponseTime(responseTime);
-        currentStimulus.setResponse(color);
-        if (color.equals(currentStimulus.getDisplayColor())) {
-            currentStimulus.setCorrect(true);
-        } else {
-            currentStimulus.setCorrect(false);
-        }
-        responses.add(currentStimulus);
-        currentStimulusIndex++;
-        if (currentStimulusIndex < stimuli.size()) {
-// present the next stimulus
-            presentStimulus(stimuli.get(currentStimulusIndex));
-        } else {
-// all stimuli have been presented, analyze the data
-            analyzeData();
-            StroopData.averageResponseTimeToCongruentStimuli(responses);
-        }
-    }
-    public void analyzeData() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(StroopActivity.this);
-        int totalCorrectResponses = StroopData.getNumberOfCorrectResponses(responses);
-        int totalIncorrectResponses = StroopData.getNumberOfIncorrectResponses(responses);
-        int totalCorrectResponsesCongruent = StroopData.getNumberOfCorrectResponsesCongruent(responses);
-        int totalIncorrectResponsesCongruent = StroopData.getNumberOfIncorrectResponsesCongruent(responses);
-        int totalCorrectResponsesNonCongruent = StroopData.getNumberOfCorrectResponsesNonCongruent(responses);
-        int totalIncorrectResponsesNonCongruent = StroopData.getNumberOfIncorrectResponsesNonCongruent(responses);
-        Map<String,Float> stimuliPercentagesPerColor = StroopData.calculatePercentageOfStimuliPerColor(responses);
-        Map<String,Float> correctResponsesPercentagesPerColor = StroopData.calculatePercentageOfCorrectResponsesPerColor(responses);
-        builder.setMessage("Total correct Responses:" + totalCorrectResponses
-                +"Total incorrect Responses:"  + totalIncorrectResponses
-                + "Total congruent correct Responses:" + totalCorrectResponsesCongruent
-        + "Total congruent incorrect Responses:" + totalIncorrectResponsesCongruent
-        + "Total Noncongruent correct Responses:" + totalCorrectResponsesNonCongruent
-        + "Total Noncongruent incorrect Responses:" + totalIncorrectResponsesNonCongruent
-        + "Stimuli percentage per color:" + stimuliPercentagesPerColor
-        + "Correct responses percentage per color:" + correctResponsesPercentagesPerColor);
+    private void onButtonClick(String colorName, int color) {
+        long responseTime = System.currentTimeMillis() - trialList.get(trialNumber - 1).getStartTime();
+        int status;
 
-        // set the positive button
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
+                if (wordTextView.getCurrentTextColor() == color) {
+                    // User chose the correct color
+                    correctCount++;
+                    resultTextView.setText("Correct: " + correctCount + " Incorrect: " + incorrectCount + " Timeout: " + timeoutCount);
+
+                    status = 1;
+                } else {
+                    // User chose the wrong color
+                    incorrectCount++;
+                    resultTextView.setText("Correct: " + correctCount + " Incorrect: " + incorrectCount + " Timeout: " + timeoutCount);
+
+                    status = 2;
+                }
+
+
+
+                // Update the trial with the response time and status
+                trialList.get(trialNumber - 1).setResponseTime(responseTime);
+                trialList.get(trialNumber - 1).setStatus(status);
+
+                // Generate a new word and color for the next test
+                setRandomWordAndColor();
             }
-        });
-// show the dialog
-        builder.show();
-        // Add any other processing of the data here
+
+    private String getColorName(int color) {
+        switch (color) {
+            case Color.RED:
+                return "RED";
+            case Color.BLUE:
+                return "BLUE";
+            case Color.GREEN:
+                return "GREEN";
+            case Color.YELLOW:
+                return "YELLOW";
+            default:
+                return "UNKNOWN";
+        }
     }
+    private void computeTestResults() {
+        int totalCorrect = 0;
+        int totalIncorrect = 0;
+        int congruentCorrect = 0;
+        int congruentIncorrect = 0;
+        int nonCongruentCorrect = 0;
+        int nonCongruentIncorrect = 0;
+        int redCount = 0;
+        int blueCount = 0;
+        int greenCount = 0;
+        int yellowCount = 0;
+        int redCorrect = 0;
+        int blueCorrect = 0;
+        int greenCorrect = 0;
+        int yellowCorrect = 0;
+        int redIncorrect = 0;
+        int blueIncorrect = 0;
+        int greenIncorrect = 0;
+        int yellowIncorrect = 0;
+        long totalResponseTime = 0;
+        long congruentResponseTime = 0;
+        long nonCongruentResponseTime = 0;
+        int congruentCount = 0;
+        int nonCongruentCount = 0;
 
+        for (Trial trial : trialList) {
+            // Count correct and incorrect responses
+            if (trial.getStatus() == 1) {
+                totalCorrect++;
+                if (trial.isColorMatch()) {
+                    congruentCorrect++;
+                } else {
+                    nonCongruentCorrect++;
+                }
+            } else {
+                totalIncorrect++;
+                if (trial.isColorMatch()) {
+                    congruentIncorrect++;
+                } else {
+                    nonCongruentIncorrect++;
+                }
+            }
 
+            // Count responses by color
+            switch (trial.getWord()) {
+                case "RED":
+                    redCount++;
+                    if (trial.getStatus() == 1) {
+                        redCorrect++;
+                    } else {
+                        redIncorrect++;
+                    }
+                    break;
+                case "BLUE":
+                    blueCount++;
+                    if (trial.getStatus() == 1) {
+                        blueCorrect++;
+                    } else {
+                        blueIncorrect++;
+                    }
+                    break;
+                case "GREEN":
+                    greenCount++;
+                    if (trial.getStatus() == 1) {
+                        greenCorrect++;
+                    } else {
+                        greenIncorrect++;
+                    }
+                    break;
+                case "YELLOW":
+                    yellowCount++;
+                    if (trial.getStatus() == 1) {
+                        yellowCorrect++;
+                    } else {
+                        yellowIncorrect++;
+                    }
+                    break;
+            }
+
+            // Calculate response times
+            long responseTime = trial.getResponseTime();
+            totalResponseTime += responseTime;
+            if (trial.isColorMatch()) {
+                congruentResponseTime += responseTime;
+                congruentCount++;
+            } else {
+                nonCongruentResponseTime += responseTime;
+                nonCongruentCount++;
+            }
+        }
+
+        // Calculate percentages and averages
+        double totalPercent = 100.0 * trialList.size() / (redCount+blueCount+greenCount+yellowCount);
+        System.out.printf("Percentage of stimuli for each color: Red: %.2f%%, Blue: %.2f%%, Green: %.2f%%, Yellow: %.2f%%\n",
+                100.0 * redCount / trialList.size(), 100.0 * blueCount / trialList.size(),
+                100.0 * greenCount / trialList.size(), 100.0 * yellowCount / trialList.size());
+        System.out.printf("Percentage of correct responses for each color: Red: %.2f%%, Blue: %.2f%%, Green: %.2f%%, Yellow: %.2f%%\n",
+                100.0 * redCorrect / redCount, 100.0 * blueCorrect / blueCount,
+                100.0 * greenCorrect / greenCount, 100.0 * yellowCorrect / yellowCount);
+        System.out.printf("Percentage of incorrect responses for each color: Red: %.2f%%, Blue: %.2f%%, Green: %.2f%%, Yellow: %.2f%%\n",
+                100.0 * redIncorrect / redCount, 100.0 * blueIncorrect / blueCount,
+                100.0 * greenIncorrect / greenCount, 100.0 * yellowIncorrect / yellowCount);
+
+        // Calculate average response times
+        double totalAvgResponseTime = (double) totalResponseTime / trialList.size();
+        double congruentAvgResponseTime = (double) congruentResponseTime / congruentCount;
+        double nonCongruentAvgResponseTime = (double) nonCongruentResponseTime / nonCongruentCount;
+        System.out.printf("Average response time to all stimuli: %.2f ms\n", totalAvgResponseTime);
+        System.out.printf("Average response time to congruent stimuli: %.2f ms\n", congruentAvgResponseTime);
+        System.out.printf("Average response time to non-congruent stimuli: %.2f ms\n", nonCongruentAvgResponseTime);
+
+        // Calculate Stroop effect
+        double stroopEffect = nonCongruentAvgResponseTime - congruentAvgResponseTime;
+        System.out.printf("Stroop effect: %.2f ms\n", stroopEffect);
+    }
 }
+
+
+
+
---
+++ app/src/main/java/com/example/stroopgame/Trial.java
@@ -0,0 +1,88 @@
+package com.example.stroopgame;
+
+public class Trial {
+    private int sequenceNumber;
+    private String word;
+    private String displayColor;
+    private boolean isColorMatch;
+    private int colorChoice;
+    private int status;
+    private long responseTime;
+    private long startTime;
+
+    public void setSequenceNumber(int sequenceNumber) {
+        this.sequenceNumber = sequenceNumber;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public void setDisplayColor(String displayColor) {
+        this.displayColor = displayColor;
+    }
+
+    public void setColorMatch(boolean colorMatch) {
+        isColorMatch = colorMatch;
+    }
+
+    public void setColorChoice(int colorChoice) {
+        this.colorChoice = colorChoice;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public void setResponseTime(long responseTime) {
+        this.responseTime = responseTime;
+    }
+
+    public Trial(int sequenceNumber, String word, String displayColor, boolean isColorMatch, int colorChoice, int status, long responseTime) {
+        this.sequenceNumber = sequenceNumber;
+        this.word = word;
+        this.displayColor = displayColor;
+        this.isColorMatch = isColorMatch;
+        this.colorChoice = colorChoice;
+        this.status = status;
+        this.responseTime = responseTime;
+    }
+
+    public int getSequenceNumber() {
+        return sequenceNumber;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getDisplayColor() {
+        return displayColor;
+    }
+
+    public boolean isColorMatch() {
+        return isColorMatch;
+    }
+
+    public int getColorChoice() {
+        return colorChoice;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public long getResponseTime() {
+        return responseTime;
+    }
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = startTime;
+    }
+
+
+}
